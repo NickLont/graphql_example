f1f1a85b751fd7fd2b551da1407a85908ed1aef8
NickLont <tormentor_gr@hotmail.com>
2020-03-25 14:56:37 +0100

    added mutations

--- server.js
+++ server.js
@@ -4,21 +4,46 @@ const graphqlHTTP = require('express-graphql')
 
 // Making a schema, using GraphQL schema language
 // All we need to do is to specify the types for our API using the GraphQL schema language, taken as an argument to the buildSchema function
-const schema = buildSchema(`
-type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-}
+// In many cases, you will find a number of different mutations that all accept the same input parameters.
+// To make your schema simpler, you can use “input types” for this, by using the input keyword instead of the type keyword.
 
-type Query {
+const schema = buildSchema(`
+  type RandomDie {
+      numSides: Int!
+      rollOnce: Int!
+      roll(numRolls: Int!): [Int]
+  }
+
+  type Query {
     hello: String,
     quoteOfTheDay: String
     random: Float!
     rollThreeDice: [Int],
     rollDice(numDice: Int!, numSides: Int): [Int],
     getDie(numSides: Int): RandomDie
-}
+  }
+`)
+
+const mutationSchema = buildSchema(`
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
+
+  type Query {
+      getMessage(id: ID!): Message
+  }
+
+  type Mutation {
+      createMessage(input: MessageInput): Message,
+      updateMessage(id: ID!, input: MessageInput): Message
+  }
 `)
 
 // Instead of a root-level resolver for the RandomDie type, we can instead use an ES6 class, where the resolvers are instance methods
@@ -40,6 +65,16 @@ class RandomDie {
     return output
   }
 }
+
+class Message {
+  constructor (id, { content, author }) {
+    this.id = id
+    this.content = content
+    this.author = author
+  }
+}
+
+const fakeDatabase = {}
 
 // The root provides a resolver function for each API endpoint
 const root = {
@@ -65,12 +100,35 @@ const root = {
   },
   getDie: ({ numSides }) => {
     return new RandomDie(numSides || 6)
+  },
+  getMessage: ({ id }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('No message with that id: ', id)
+    }
+    return new Message(id, fakeDatabase[id])
+  },
+  createMessage: ({ input }) => {
+    const id = require('crypto').randomBytes(10).toString('hex')
+    fakeDatabase[id] = input
+    return new Message(id, input)
+  },
+  updateMessage: ({ id, input }) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('No message with that id: ', id)
+    }
+    fakeDatabase[id] = input
+    return new Message(id, input)
   }
 }
 
 const app = express()
 app.use('/graphql', graphqlHTTP({
   schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
+app.use('/graphqlmutations', graphqlHTTP({
+  schema: mutationSchema,
   rootValue: root,
   graphiql: true
 }))
